59df27bc4e4b9a8b3ca12bc0701a8158635a1ddc
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-08-04 15:47:55 +0300

    feat: container component has been updated

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import Content from './Content'
 import Sidebar from './Sidebar'
 
 
 const Container = () => {
   return (
-    <div className="grid h-screen place-items-center grid-cols-5">
-      <div className='col-start-2'>
-        <Sidebar />
+    <div className="grid h-screen place-content-center grid-cols-5 gap-4">
+      <div/>
+      <div className='place-items-center'><Sidebar /></div>
+      <div className='col-span-2'>
+        <Content />
       </div>
+      <div></div>
     </div>
   )
 }
