3dc269854e818867785201fd5ce144cbde5e1e77
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-08-04 13:07:42 +0300

    feat: social media components has been created

---
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+    BsLinkedin,
+    BsGithub,
+    BsTwitter,
+    BsMedium
+} from 'react-icons/bs'
+import SocialMediaItem from './SocialMediaItem'
+
+
+const SocialMedia = () => {
+    return (
+        <div class="grid h-20 place-items-center grid-cols-4 rounded-md shadow-sm" role="group">
+            <SocialMediaItem icon={<BsLinkedin color='blue' />} />
+            <SocialMediaItem icon={<BsGithub color='black' />} />
+            <SocialMediaItem icon={<BsTwitter color='blue' />} />
+            <SocialMediaItem icon={<BsMedium color='black' />} />
+        </div>
+    )
+}
+
+export default SocialMedia
