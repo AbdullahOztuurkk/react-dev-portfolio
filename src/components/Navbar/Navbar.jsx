a417785bb7d24cc93c52a13c26ed639101674778
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-08-04 17:32:23 +0300

    feat: navbar component has been moved into one folder

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import NavbarItem from './Navbar/NavbarItem'
+import NavbarItem from './NavbarItem'
 
 const Navbar = () => {
     return (
 
         <React.Fragment>
-            <ul class="text-sm font-medium text-center text-gray-500 rounded-lg divide-x divide-gray-200 shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
+            <ul className="text-sm font-medium text-center text-gray-500 rounded-lg divide-x divide-gray-200 shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
                 <NavbarItem name="Experiences" link='/' isCurrent={true} />
                 <NavbarItem name="Services" link='/' />
                 <NavbarItem name="Testimonials" link='/' />
