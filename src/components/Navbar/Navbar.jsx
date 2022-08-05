e569953e405c006f6f4087e8f04dc784596df075
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-08-05 17:06:39 +0300

    refactor: get url method has been created

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,20 +3,22 @@ import { useLocation } from 'react-router-dom'
 import NavbarItem from './NavbarItem'
 
 const Navbar = () => {
-    const location = useLocation();
-    const { pathname } = location;
-    const splitLocation = pathname.split("/");
-    console.log(splitLocation);
-
+    
+    const GetUrl = () =>{
+        const location = useLocation();
+        const { pathname } = location;
+        const splitLocation = pathname.split("/");
+        return splitLocation[1];
+    }
 
     return (
 
         <React.Fragment>
             <ul className="text-sm font-medium text-center text-gray-500 rounded-lg divide-x divide-gray-200 shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
-                <NavbarItem name="Experiences" link='/experiences' isCurrent={splitLocation[1] === 'experiences' } />
-                <NavbarItem name="Services" link='/services' isCurrent={splitLocation[1] === 'services'} />
-                <NavbarItem name="Testimonials" link='/testimonials' isCurrent={splitLocation[1] === 'testimonials'} />
-                <NavbarItem name="Contact" link='/contact' isCurrent={splitLocation[1] === 'contact'} />
+                <NavbarItem name="Experiences" link='/experiences' isCurrent={GetUrl() === 'experiences' || GetUrl() === "" } />
+                <NavbarItem name="Services" link='/services' isCurrent={GetUrl() === 'services'} />
+                <NavbarItem name="Testimonials" link='/testimonials' isCurrent={GetUrl() === 'testimonials'} />
+                <NavbarItem name="Contact" link='/contact' isCurrent={GetUrl() === 'contact'} />
             </ul>
         </React.Fragment>
     )
