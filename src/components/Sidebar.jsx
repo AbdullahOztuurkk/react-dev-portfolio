9ac23682941ff747d3f376bfa0287ac30ce73cdf
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-08-05 17:57:00 +0300

    feat: biography component has been added into the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Biography from './Biography/Biography'
 import SocialMedia from './SocialMedia/SocialMedia'
 
 const Sidebar = () => {
@@ -10,7 +11,7 @@ const Sidebar = () => {
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">Abdullah Öztürk</h5>
                 </a>
                 <p className="mb-3 font-normal text-center text-gray-900">Software Developer</p>
-                <p className='mt-5 mb-5 font-normal text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eius odit quam nemo! Consectetur, tenetur? Voluptates blanditiis quibusdam vitae veritatis odit? Doloribus fuga, itaque neque vel id aliquam laborum? Dolorum.</p>
+                <Biography />
                 <SocialMedia />
             </div>
         </div>
