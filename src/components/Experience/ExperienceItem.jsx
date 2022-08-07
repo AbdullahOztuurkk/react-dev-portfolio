5bc829e2123b5c0abae367e74f14a4edf0296bff
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-08-07 20:17:17 +0300

    feat: some experinceitem component improvements

--- src/components/Experience/ExperienceItem.jsx
+++ src/components/Experience/ExperienceItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ExperienceItem = ({ companyName, date, description, title, imageLink/*, link*/ }) => {
     return (
-        <div class="flex items-center lg:w-full mx-auto border-b pb-5 mb-5 border-gray-200 sm:flex-row flex-col">
+        <div class="flex items-center lg:w-full mx-auto pb-5 pt-5 mb-5 sm:flex-row flex-col">
           <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full text-red-500 flex-shrink-0">
             <img src={imageLink  == null ? "/logo512.png" : imageLink} width={150} height={150} />
           </div>
@@ -11,11 +11,12 @@ const ExperienceItem = ({ companyName, date, description, title, imageLink/*, li
             <h3 class="text-gray-900 text-lg title-font font-serif mb-1">{companyName}</h3>
             <h5 className='text-gray-900 text-base title-font font-mono mb-1'>{date}</h5>
             <p class="leading-relaxed text-base">{description}</p>
-            <a class="mt-3 text-red-500 inline-flex items-center">Learn More
+            {/* If you want to use link, uncomment link from above and give link as props. Also dont forget to uncomment this code block*/}
+            {/* <a class="mt-3 text-red-500 inline-flex items-center">Learn More
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
               </svg>
-            </a>
+            </a> */}
           </div>
         </div>
     )
