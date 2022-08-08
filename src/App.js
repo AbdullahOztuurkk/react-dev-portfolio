c88f686d49aafdbe758e3d17c0dd7e6edd5f1db7
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-08-08 15:03:01 +0300

    feat: added config context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Container from "./components/Container";
+import ConfigContext from "./context/ConfigContext";
+import data from "./config.json";
 
 const App = () => {
-	return <Container />;
+	return (
+		<ConfigContext.Provider value={data}>
+			<Container />
+		</ConfigContext.Provider>
+	)
 };
 
 export default App;
---
+++ src/context/ConfigContext.jsx
@@ -0,0 +1,5 @@
+import React, { createContext } from 'react'
+
+const ConfigContext = createContext();
+
+export default ConfigContext
