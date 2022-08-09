26afd3d53a0da0acd7d4ead56ce2467d61cfa812
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-08-09 12:58:38 +0300

    feat: redesigned testimonials page according to config file

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import TestimonialsItem from './TestimonialsItem'
 
-const Testimonials = () => {
+const Testimonials = ({ value }) => {
   return (
     <section class="text-gray-600 body-font">
       <div class="container px-5 py-6 mx-auto">
         <h1 class="text-3xl font-medium title-font text-gray-900 mb-6 text-center">Testimonials</h1>
         <h3 className='text-xl text-gray-900 mb-6 text-center'>Customers who benefits provided services by me</h3>
         <div class="flex flex-wrap -m-4">
-          <TestimonialsItem name="Abdullah Öztürk" title="Software Developer" content={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, accusantium commodi "} />
-          <TestimonialsItem name="John Doe" title="Software Development Lead" content={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, accusantium commodi. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, accusantium commodi "} />
+          {value.map((testimonial) =>
+            <TestimonialsItem name={testimonial.name} title={testimonial.title} content={testimonial.content} imageLink={testimonial.imageLink} />
+          )}
         </div>
       </div>
     </section>
